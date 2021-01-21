1612dbcc881ad729c962ce4c1817ebf65973c2b6
Yuya Ohbayashi <yuya9445@gmail.com>
2021-01-21 16:58:57 +0900

    147:TableとCenterでスコア表示部分を実装

--- lib/screens/testScreen.dart
+++ lib/screens/testScreen.dart
@@ -9,6 +9,10 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen> {
+  int numberOfRemaining;
+  int numberOfCorrect;
+  int correctRate;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -33,26 +37,46 @@ class _TestScreenState extends State<TestScreen> {
 
   //スコア表示部分
   Widget _scorePart() {
-
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+      child: Table(
+        children: [
+          TableRow(
+            children: [
+              Center(child: Text("残り問題数", style: TextStyle(fontSize: 10.0),)),
+              Center(child: Text("正解数", style: TextStyle(fontSize: 10.0),)),
+              Center(child: Text("正答率", style: TextStyle(fontSize: 10.0),)),
+            ]
+          ),
+          TableRow(
+            children: [
+              Center(child: Text(numberOfRemaining.toString(), style: TextStyle(fontSize: 18.0),)),
+              Center(child: Text(numberOfCorrect.toString(), style: TextStyle(fontSize: 18.0),)),
+              Center(child: Text(correctRate.toString(), style: TextStyle(fontSize: 18.0),)),
+            ]
+          ),
+        ]
+      ),
+    );
   }
 
   //問題表示部分
   Widget _questionPart() {
-
+    return Container();
   }
 
   //電卓ボタン部分
   Widget _calcButtons() {
-
+    return Container();
   }
 
   //答え合わせボタン
   Widget _answerCheckButton() {
-
+    return Container();
   }
 
   //戻るボタン
   Widget _backButton() {
-
+    return Container();
   }
 }
