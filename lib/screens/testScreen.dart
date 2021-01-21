b810c448b2612cc1db83eb3b926808929767556e
Yuya Ohbayashi <yuya9445@gmail.com>
2021-01-21 16:36:03 +0900

    146:画面全体のレイアウトの枠組みを作る

--- lib/screens/testScreen.dart
+++ lib/screens/testScreen.dart
@@ -11,8 +11,48 @@ class TestScreen extends StatefulWidget {
 class _TestScreenState extends State<TestScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text(widget.numberOfQuestions.toString())),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            //スコア表示部分
+            _scorePart(),
+            //問題表示部分
+            _questionPart(),
+            //電卓ボタン部分
+            _calcButtons(),
+            //答え合わせボタン
+            _answerCheckButton(),
+            //戻るボタン
+            _backButton(),
+          ],
+        )
+      ),
     );
+  }
+
+  //スコア表示部分
+  Widget _scorePart() {
+
+  }
+
+  //問題表示部分
+  Widget _questionPart() {
+
+  }
+
+  //電卓ボタン部分
+  Widget _calcButtons() {
+
+  }
+
+  //答え合わせボタン
+  Widget _answerCheckButton() {
+
+  }
+
+  //戻るボタン
+  Widget _backButton() {
+
   }
 }
